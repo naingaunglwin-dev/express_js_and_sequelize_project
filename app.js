5bd6f55497ddb7d98206758ecdb7a707102d6f10
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2026-02-03 15:31:14 +0630

    feat: setup api v1 route in app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const config = require("./config/app");
+const route = require("./routes/api");
 
 const app = express();
+
+route.mountApiV1(app);
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
