c54b3f068c864adfe45ebdbf36b1fdb04f77bb44
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2026-02-03 15:35:13 +0630

    feat: add user, project, token migration and model files

---
+++ models/token.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Token extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Token.init({
+    user_id: DataTypes.INTEGER,
+    token_type: DataTypes.STRING,
+    token: DataTypes.STRING,
+    token_expires_at: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Token',
+  });
+  return Token;
+};
